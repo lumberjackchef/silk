bf7eaf671a65ad4e787ad202cae9ac8047e1edd6
Ryan Pearson <ryan@ryanpearson.net>
2019-05-23 11:23:44 -0700

    Adds version checking/updating commands

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
   "encoding/json"
   "fmt"
+  "io/ioutil"
   "log"
   "os"
   "time"
@@ -10,16 +11,16 @@ import (
   "github.com/urfave/cli"
 )
 
-type project_meta struct {
-  ProjectName  string   `json:"project_name"`
-  InitDate     string   `json:"init_date"`
+type ProjectMeta struct {
+  ProjectName string  `json:"project_name"`
+  InitDate    string  `json:"init_date"`
+  Version     string  `json:"version"`
 }
 
 func main() {
   app := cli.NewApp()
   app.Name = "silk"
   app.Usage = "A modern version control paradigm for service oriented architectures."
-  app.Version = "0.0.0"
 
   app.Commands = []cli.Command{
     {
@@ -33,17 +34,18 @@ func main() {
           if c.NArg() > 0 {
             os.Mkdir(path, 0766)
 
-            f_config, err := os.Create(".silk/config")
+            f_meta, err := os.Create(".silk/meta.json")
             check(err)
-            defer f_config.Close()
+            defer f_meta.Close()
 
             dt := time.Now().String()
 
-            project_meta_data, _ := json.Marshal(&project_meta{
+            project_meta_data, _ := json.MarshalIndent(&ProjectMeta{
               ProjectName:  fmt.Sprintf(c.Args().Get(0)),
               InitDate:     dt,
-            })
-            _, err2 := f_config.WriteString(string(project_meta_data))
+              Version:      "0.0.0",
+            }, "", "  ")
+            _, err2 := f_meta.WriteString(string(project_meta_data) + "\n")
             check(err2)
 
             fmt.Println("New project created!")
@@ -80,6 +82,49 @@ func main() {
       Usage: "If no arguments, lists all components in the current project. If a name is supplied, this will either move to the component, clone from remote & move to the component, or it will create a new component of name [name]",
       Action: func(c *cli.Context) error {
         commandAction(func() { fmt.Println("Coming Soon!") })
+        return nil
+      },
+    },
+    {
+      Name: "version",
+      Aliases: []string{"v"},
+      Usage: "Lists or edits the current version of the project",
+      Action: func(c *cli.Context) error {
+        commandAction(
+          func() {
+            var meta_data ProjectMeta
+
+            // Open, check, & defer closing of the meta data file
+            meta_file, meta_file_err := os.Open(".silk/meta.json")
+            check(meta_file_err)
+            defer meta_file.Close()
+
+            // Get the []byte version of the json data
+            byte_value, byte_value_err := ioutil.ReadAll(meta_file)
+            check(byte_value_err)
+
+            // Transform the []byte data into usable struct data
+            meta_data_err := json.Unmarshal(byte_value, &meta_data)
+            check(meta_data_err)
+
+            if c.NArg() > 0 {
+              // Change the version & transform back to actual json
+              meta_data.Version = fmt.Sprintf(c.Args().Get(0))
+              meta_data_json, meta_data_json_err := json.MarshalIndent(meta_data, "", " ")
+              check(meta_data_json_err)
+
+              // Write the version change to the file
+              meta_file_write_err := ioutil.WriteFile(".silk/meta.json", []byte(string(meta_data_json) + "\n"), 0766)
+              check(meta_file_write_err)
+
+              // Confirmation message
+              fmt.Println("Version successfull updated to " + meta_data.Version + "!")
+            } else {
+              // If the user just wants to check the version and not change it
+              fmt.Println(meta_data.Version)
+            }
+          },
+        )
         return nil
       },
     },
