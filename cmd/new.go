85bde26207b3642a61c68e84a4c988195302e24e
Ryan <ryan@ryanpearson.net>
2019-06-22 22:55:37 -0700

    Remvoves erroneously added CommandAction for new project creation

--- cmd/new.go
+++ cmd/new.go
@@ -22,34 +22,33 @@ func New() cli.Command {
 		Aliases: []string{"n"},
 		Usage:   "Create a new silk project",
 		Action: func(c *cli.Context) error {
-			helper.CommandAction(func() {
-				// Colors setup
-				cNotice := color.New(color.FgGreen).SprintFunc()
-				cWarning := color.New(color.FgYellow).SprintFunc()
+			// Colors setup
+			cNotice := color.New(color.FgGreen).SprintFunc()
+			cWarning := color.New(color.FgYellow).SprintFunc()
 
-				// Project tracking folder. This checks if the folder exists, creates it if not.
-				// TODO: implement checking similar to SilkRoot() to ensure we're not within the bounds of another project
-				if _, err := os.Stat(helper.RootDirectoryName); os.IsNotExist(err) {
-					if c.NArg() > 0 {
-						// Creates the silk directory
-						os.Mkdir(helper.RootDirectoryName, 0766)
+			// Project tracking folder. This checks if the folder exists, creates it if not.
+			// TODO: implement checking similar to SilkRoot() to ensure we're not within the bounds of another project
+			if _, err := os.Stat(helper.RootDirectoryName); os.IsNotExist(err) {
+				if c.NArg() > 0 {
+					// Creates the silk directory
+					os.Mkdir(helper.RootDirectoryName, 0766)
 
-						projectName := fmt.Sprintf(c.Args().Get(0))
+					projectName := fmt.Sprintf(c.Args().Get(0))
 
-						// Create relevant project files
-						helper.CreateRootMetaFile(projectName)
-						helper.CreateComponentList(projectName)
-						helper.CreateInitialCommitBuffer()
+					// Create relevant project files
+					helper.CreateRootMetaFile(projectName)
+					helper.CreateComponentList(projectName)
+					helper.CreateInitialCommitBuffer()
 
-						// Confirmation message
-						fmt.Println("\tNew project " + cNotice(fmt.Sprintf(c.Args().Get(0))) + " created!")
-					} else {
-						fmt.Printf("\t%s No project name specified!\n", cWarning("Warning:"))
-					}
+					// Confirmation message
+					fmt.Println("\tNew project " + cNotice(fmt.Sprintf(c.Args().Get(0))) + " created!")
 				} else {
-					fmt.Printf("\t%s this is an existing silk project!\n", cWarning("Warning:"))
+					fmt.Printf("\t%s No project name specified!\n", cWarning("Warning:"))
 				}
-			})
+			} else {
+				fmt.Printf("\t%s this is an existing silk project!\n", cWarning("Warning:"))
+			}
+
 			return nil
 		},
 	}
