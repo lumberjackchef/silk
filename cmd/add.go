65e9dea0c3068da0cbc555bafc9883019da356a7
Ryan <ryan@ryanpearson.net>
2019-07-02 22:35:14 -0700

    Adds interactive addition placeholder and starts silk add command functionality

--- cmd/add.go
+++ cmd/add.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/fatih/color"
@@ -11,15 +12,27 @@ import (
 // Add allows for the addition of changes to the commit buffer
 func Add() cli.Command {
 	cNotice := color.New(color.FgGreen).SprintFunc()
+	cWarning := color.New(color.FgYellow).SprintFunc()
 
 	return cli.Command{
 		Name:  "add",
 		Usage: "Adds a file or files to the current commit buffer",
 		Action: func(c *cli.Context) error {
-			// TODO: add interactive addition for multifile additions
-			// TODO: add simple, single file/folder change additions to the commit buffer
-			helper.CommandAction(func() { fmt.Printf("\t%s\n", cNotice("Coming Soon!")) })
+			helper.CommandAction(func() {
+				if c.NArg() > 0 {
+					fmt.Printf("\t%s\n", cNotice("Coming Soon!"))
+					// TODO: check against changes not in the commit buffer
+					// TODO: see if the listed item is a valid file/directory
+					// TODO: add file/directory changes to commit buffer
+				} else {
+					err := errors.New("no files specified")
+					fmt.Println("\n\t" + cWarning("Warning: ") + err.Error() + ". Please specify which files/changes you wish to add.\n")
+				}
+			})
 			return nil
+		},
+		Subcommands: []cli.Command{
+			InteractiveAddition(),
 		},
 	}
 }
---
+++ cmd/add_interactive.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+	"github.com/lumberjackchef/silk/helper"
+	"github.com/urfave/cli"
+)
+
+// InteractiveAddition allows for the addition of a large number of changes to the commit buffer
+func InteractiveAddition() cli.Command {
+	cNotice := color.New(color.FgGreen).SprintFunc()
+
+	return cli.Command{
+		Name:    "interactive",
+		Aliases: []string{"i"},
+		Usage:   "Adds a large amount of changes to the current commit buffer via an interactive interface",
+		Action: func(c *cli.Context) error {
+			helper.CommandAction(func() { fmt.Printf("\t%s\n", cNotice("Coming Soon!")) })
+			return nil
+		},
+	}
+}
