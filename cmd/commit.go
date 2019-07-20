c7dffbbd5553870941c3e6ee90178177d2635d6e
Ryan <ryan@ryanpearson.net>
2019-07-20 16:56:47 -0700

    Updates commit functionality to remove committed files from buffer

--- cmd/commit.go
+++ cmd/commit.go
@@ -28,13 +28,31 @@ type CommitFile struct {
 // Commit adds commit buffer data to a commit file
 func Commit() cli.Command {
 	cNotice := color.New(color.FgGreen).SprintFunc()
+	cWarning := color.New(color.FgYellow).SprintFunc()
 
 	return cli.Command{
 		Name:  "commit",
 		Usage: "Tags the current commit buffer and resets all file statuses",
 		Action: func(c *cli.Context) error {
-			commitFile, err := os.Create(helper.RootDirectoryName + "/commit/latest")
-			helper.Check(err)
+			var commitFile *os.File
+			latestCommittFile := helper.SilkRoot() + "/" + helper.RootDirectoryName + "/commit/latest"
+
+			// TODO: check if there are changes in the commit buffer
+
+			if _, err := os.Stat(latestCommittFile); os.IsNotExist(err) {
+				commitFile, err = os.Create(latestCommittFile)
+				if err != nil {
+					fmt.Println(cWarning("\n\tError") + ": unable to create latest commit file")
+					fmt.Print("\n")
+				}
+			} else {
+				commitFile, err = os.OpenFile(latestCommittFile, os.O_APPEND|os.O_RDWR, os.ModeAppend)
+				if err != nil {
+					fmt.Println(cWarning("\n\tError") + ": unable to open latest commit file")
+					fmt.Print("\n")
+				}
+			}
+
 			defer commitFile.Close()
 
 			commitFileData, _ := json.MarshalIndent(
@@ -52,25 +70,40 @@ func Commit() cli.Command {
 				"	",
 			)
 
-			_, err = commitFile.WriteString(string(commitFileData) + "\n")
-			helper.Check(err)
+			_, err := commitFile.WriteString(string(commitFileData) + "\n")
+			if err != nil {
+				fmt.Println(cWarning("\n\tError") + ": unable to write to commit file")
+				fmt.Print("\n")
+			}
 
 			hash := sha1.New()
 			if _, err := io.Copy(hash, commitFile); err != nil {
 				log.Fatal(err)
+				if err != nil {
+					fmt.Println(cWarning("\n\tError") + ": unable to copy commit file contents to hash")
+					fmt.Print("\n")
+				}
 			}
 			sum := hash.Sum(nil)
 			str := hex.EncodeToString(sum)
 
 			hashedCommitFile, err := os.Create(helper.RootDirectoryName + "/commit/" + str)
-			helper.Check(err)
+			if err != nil {
+				fmt.Println(cWarning("\n\tError") + ": unable to create hashed commit file")
+				fmt.Print("\n")
+			}
 			defer hashedCommitFile.Close()
 
 			// TODO: compress these files (brotli?)
 			_, err = hashedCommitFile.Write(commitFileData)
-			helper.Check(err)
+			if err != nil {
+				fmt.Println(cWarning("\n\tError") + ": unable to write to hashed commit file")
+				fmt.Print("\n")
+			}
 
 			// TODO: remove committed files from buffer somehow
+			helper.ClearCommitBuffer()
+
 			fmt.Println("\n\t" + cNotice("Success!"))
 			fmt.Print("\n")
 			// TODO: find a way to identify merge commits clearly, AWAITING branching commands creation
--- helper/commit_buffer_change.go
+++ helper/commit_buffer_change.go
@@ -3,8 +3,11 @@ package helper
 import (
 	"bufio"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"os"
+
+	"github.com/fatih/color"
 )
 
 // AddFileToCommitBuffer ...
@@ -12,20 +15,33 @@ import (
 func AddFileToCommitBuffer(fileName string) {
 	var commitBuffer RootCommitBuffer
 	var changes []FileChange
+	cWarning := color.New(color.FgYellow).SprintFunc()
 	oldChanges := CommitBuffer().Changes
 
 	bufferFile, err := os.Open(SilkRoot() + "/" + RootDirectoryName + "/commit/buffer")
-	Check(err)
+	if err != nil {
+		fmt.Println(cWarning("\n\tError") + ": unable to open buffer file")
+		fmt.Print("\n")
+	}
 	defer bufferFile.Close()
 
 	byteValue, err := ioutil.ReadAll(bufferFile)
-	Check(err)
+	if err != nil {
+		fmt.Println(cWarning("\n\tError") + ": unable to read buffer file byte value")
+		fmt.Print("\n")
+	}
 
 	err = json.Unmarshal(byteValue, &commitBuffer)
-	Check(err)
+	if err != nil {
+		fmt.Println(cWarning("\n\tError") + ": unable to unmarshal commit buffer")
+		fmt.Print("\n")
+	}
 
 	changedFile, err := os.Open(SilkRoot() + "/" + fileName)
-	Check(err)
+	if err != nil {
+		fmt.Println(cWarning("\n\tError") + ": unable to open file")
+		fmt.Print("\n")
+	}
 	defer changedFile.Close()
 
 	scanner := bufio.NewScanner(changedFile)
@@ -46,8 +62,54 @@ func AddFileToCommitBuffer(fileName string) {
 	commitBuffer.Changes = changes
 
 	commitBufferJSON, err := json.MarshalIndent(commitBuffer, " ", "")
-	Check(err)
+	if err != nil {
+		fmt.Println(cWarning("\n\tError") + ": unable to marshal json to commit buffer")
+		fmt.Print("\n")
+	}
 
 	err = ioutil.WriteFile(SilkRoot()+"/.silk/commit/buffer", []byte(string(commitBufferJSON)+"\n"), 0766)
-	Check(err)
+	if err != nil {
+		fmt.Println(cWarning("\n\tError") + ": unable to write to commit buffer file")
+		fmt.Print("\n")
+	}
+}
+
+// ClearCommitBuffer removes all entries from the commit buffer
+func ClearCommitBuffer() {
+	var commitBuffer RootCommitBuffer
+	cWarning := color.New(color.FgYellow).SprintFunc()
+
+	bufferFile, err := os.Open(SilkRoot() + "/" + RootDirectoryName + "/commit/buffer")
+	if err != nil {
+		fmt.Println(cWarning("\n\tError") + ": unable to open commit buffer file")
+		fmt.Print("\n")
+	}
+	defer bufferFile.Close()
+
+	// Remove all bufferFile.Changes()
+	byteValue, err := ioutil.ReadAll(bufferFile)
+	if err != nil {
+		fmt.Println(cWarning("\n\tError") + ": unable to read buffer file byte values")
+		fmt.Print("\n")
+	}
+
+	err = json.Unmarshal(byteValue, &commitBuffer)
+	if err != nil {
+		fmt.Println(cWarning("\n\tError") + ": unable to create latest commit file")
+		fmt.Print("\n")
+	}
+
+	commitBuffer.Changes = []FileChange{}
+
+	commitBufferJSON, err := json.MarshalIndent(commitBuffer, " ", "")
+	if err != nil {
+		fmt.Println(cWarning("\n\tError") + ": unable to marshal json to commit buffer")
+		fmt.Print("\n")
+	}
+
+	err = ioutil.WriteFile(SilkRoot()+"/.silk/commit/buffer", []byte(string(commitBufferJSON)+"\n"), 0766)
+	if err != nil {
+		fmt.Println(cWarning("\n\tError") + ": unable to write to commit buffer file")
+		fmt.Print("\n")
+	}
 }
